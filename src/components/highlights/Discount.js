2e10e39c5d5e77664d8b377b0ceaed41eb9fafc7
Edgar <amirbekianedgar@gmail.com>
2019-06-20 20:38:28 +0400

    add discount and finalize the highlights section

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -1,29 +1,58 @@
 import React from 'react';
 
+import MyButton from '../utils/MyButton';
+
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
+
+
 class Discount extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            percent: 0
+            discountStart: 0,
+            discountEnd: 30
         }
     }
 
-    incrementUntil = (val) => {
-        this.setState({
-            percent: val + 1
-        })
+    incrementUntil = () => {
+        let {discountStart, discountEnd} = this.state;
+        if (discountStart < discountEnd) {
+            this.setState({
+                discountStart: discountStart + 1
+            })
+        }
     };
 
-    componentDidMount() {
-        while (this.state.percent <= 30) {
-            setInterval(() => this.incrementUntil(this.state.percent), 500)
-        }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        setTimeout(() => this.incrementUntil(), 30)
     }
 
     render() {
         return (
-            <div>
-                {this.state.percent}
+            <div className="center_wrapper">
+                <div className="discount_wrapper">
+                    <Fade
+                        onReveal={() => {this.incrementUntil()}}
+                    >
+                        <div className="discount_porcentage">
+                            <span>{this.state.discountStart}%</span>
+                            <span>OFF</span>
+                        </div>
+                    </Fade>
+                    <Slide right>
+                        <div className="discount_description">
+                            <h3> Purchase tickets before 20 July </h3>
+                            <p> Sed eget tristique ipsum. Donec eget tortor metus. Nulla nisi velit, pulvinar sit amet arcu non, interdum tincidunt enim. Pellentesque malesuada nunc libero, id elementum tortor porttitor elementum. Vestibulum porttitor interdum eros, posuere rutrum massa rutrum in. Cras id purus et est placerat blandit. Integer hendrerit mauris vitae massa efficitur, sit amet volutpat metus scelerisque. Pellentesque aliquet dolor sem. Maecenas sit amet nunc ipsum. Nam viverra libero sit amet neque mattis, ut interdum nisi dictum. Proin vel ex quis orci dictum ultricies. Sed quis nulla finibus, euismod odio hendrerit, elementum tortor.  </p>
+                            <MyButton
+                                text="Purchase Tickets"
+                                bck="#ffa800"
+                                color="#ffffff"
+                                link="https://google.com"
+                            />
+                        </div>
+                    </Slide>
+                </div>
             </div>
         );
     }
